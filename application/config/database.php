3d328ed810cc6bc1d8f65e298dc096a685c9f624
Jerubaalking <gsainyeye@gmail.com>
2023-08-15 23:37:28 +0300

    update

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => 'thina@2023',
 	'database' => 'school_db',
-	'dbdriver' => 'mysql',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
